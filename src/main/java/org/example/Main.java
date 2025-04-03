7067a59fbbf85834758c65fb55a2bc7d7303701f
SAROJKARKI0321 <sarojkarki0321@gmail.com>
2025-04-03 10:39:50 -0500

    just commments for program working

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ public class Main {
         int[] nums = {3, 5, 6};
         try {
             System.out.println(nums[4]/1);
+            // use 0 instead of 1 to get Something went wrong and use nums[any outside number] to get out of bound exceptions.
         } catch (IndexOutOfBoundsException e) {
             System.out.println("Out of Index");
         } catch (Exception E) {
