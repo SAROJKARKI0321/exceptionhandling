1071f117dac7bf23de6839e7087fcc53fce5d5b5
SAROJKARK0321 <sarojkarki0321@gmail.com>
2025-04-03 10:31:04 -0500

    Basic exception handling

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {3, 5, 6};
+        try {
+            System.out.println(nums[4]/1);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Out of Index");
+        } catch (Exception E) {
+            System.out.println("Something went wrong");
+        }
+    }
+}
