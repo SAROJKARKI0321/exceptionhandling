504b9c733ca01be70f28e0707c31b666c61f7e76
SAROJKARK0321 <sarojkarki0321@gmail.com>
2025-04-03 10:58:51 -0500

    custom exception

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,17 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        int[] nums = {3, 5, 6};
-        try {
-            System.out.println(nums[4]/1);
-            // use 0 instead of 1 to get Something went wrong and use nums[any outside number] to get out of bound exceptions.
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("Out of Index");
-        } catch (Exception E) {
-            System.out.println("Something went wrong");
+    public static void checkNationality ( String country){
+        if(country.toLowerCase().equals("nepal")){
+            System.out.println("National");
+        }else {
+            throw  new IllegalArgumentException("*****International****");
+
         }
+
+    }
+
+    public static void main(String[] args) {
+        checkNationality("nepal");
     }
 }
